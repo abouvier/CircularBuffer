c61715ff6391a29f91ab155cab755bac1edfbf37
abouvier <abouvier@student.42.fr>
2015-08-31 00:53:39 +0200

    change licence

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -1,14 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   CircularBuffer.h                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: abouvier <abouvier@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/08/14 18:01:48 by abouvier          #+#    #+#             */
-/*   Updated: 2015/08/18 07:17:03 by abouvier         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+	CircularBuffer.h - Circular buffer library for Arduino
+	Copyright 2015 abouvier <abouvier@student.42.fr>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 
 #ifndef CIRCULARBUFFER_H
 #define CIRCULARBUFFER_H
